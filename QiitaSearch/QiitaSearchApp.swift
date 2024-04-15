17baa456d462c92afe21d788ca657886a9004048
RyunosukeFunasako <e205732@ie.u-ryukyu.ac.jp>
2024-04-15 15:06:15 +0900

    Initial Commit

---
+++ QiitaSearch/QiitaSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiitaSearchApp.swift
+//  QiitaSearch
+//
+//  Created by 船迫龍之介 on 2024/04/15.
+//
+
+import SwiftUI
+
+@main
+struct QiitaSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
