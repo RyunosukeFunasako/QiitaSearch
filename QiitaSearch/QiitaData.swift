93644336c53a1a0feab1283b55a4d8de12469de7
RyunosukeFunasako <e205732@ie.u-ryukyu.ac.jp>
2024-04-15 15:13:40 +0900

    first commit

---
+++ QiitaSearch/QiitaData.swift
@@ -0,0 +1,8 @@
+//
+//  QiitaData.swift
+//  QiitaSearch
+//
+//  Created by 船迫龍之介 on 2024/04/15.
+//
+
+import Foundation
---
+++ QiitaSearch/SafariView.swift
@@ -0,0 +1,8 @@
+//
+//  SafariView.swift
+//  QiitaSearch
+//
+//  Created by 船迫龍之介 on 2024/04/15.
+//
+
+import Foundation
